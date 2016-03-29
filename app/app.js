6b91d56d6277a791aa02de990e4627c8532f3110
JeremyCarlsten <jeremy@jeremycarlsten.com>
2016-03-29 07:35:24 -0500

    adding the sidebar information

--- app/app.js
+++ app/app.js
@@ -7,13 +7,20 @@
 
   function UserSearchController(searchService) {
       var self = this;
-
+      self.user = {};
       activate();
 
       function activate() {
           return searchService.getAllUserData().then(function(data){
                   self.userData = data;
                 });
+      }
+
+      self.getUserDetails = function(index){
+        console.log(self.userData[index]);
+        if(self.userData[index]){
+          self.user = self.userData[index];
+        }
       }
   }
 })();
